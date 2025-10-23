d2049ec38b97af74cc5eba494db3a83edfad2e3e
AbdulRafay575 <136607014+AbdulRafay575@users.noreply.github.com>
2025-10-23 21:42:37 +0500

    cors issue for fortex online

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -109,7 +109,6 @@ const prepareBankPayment = (order) => {
 // @route   POST /api/orders/payment-callback
 // @access  Public
 console.log('🔍 Verifying callback hash using Hashv3...');
-console.log("🔙 Bank Callback Data:", req.body);
 
 const storeKey = process.env.BANK_STORE_KEY || 'SKEY0335';
 
